e28aed477ea4b61ca825d7903f58f151ff00aa8a
NLS17-MAC <tushar.mistri@nexuslinkservices.in>
2018-11-22 12:42:44 +0530

    Initial commit

---
+++ PickyPodWed/Classes/PickyTesterPod.swift
@@ -0,0 +1,17 @@
+//
+//  PickyTesterPod.swift
+//  PickyPodWed
+//
+//  Created by NLS17-MAC on 11/22/18.
+//
+
+import UIKit
+
+public class PickyTesterPod: NSObject {
+
+    public func pickTestLog()
+    {
+        print("Hello ....")
+    }
+    
+}
